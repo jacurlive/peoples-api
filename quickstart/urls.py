87044d58eb568ec8a198c9e67edd0759c2699cec
jacurlive <jasurkkirchh@gmail.com>
2023-12-04 14:26:38 +0500

    fix urls

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -3,8 +3,8 @@ from .views import CategoryAPIView, PeopleAPIList, PeopleAPIUpdate, PeopleAPIDet
 
 
 urlpatterns = [
-    path('api/v1/peoples', PeopleAPIList.as_view()),
-    path('api/v1/category', CategoryAPIView.as_view()),
-    path('api/v1/peoples/<int:pk>', PeopleAPIUpdate.as_view()),
-    path('api/v1/peoplesdetail/<int:pk>', PeopleAPIDetail.as_view())
+    path('api/v1/peoples/', PeopleAPIList.as_view()),
+    path('api/v1/category/', CategoryAPIView.as_view()),
+    path('api/v1/peoples/<int:pk>/', PeopleAPIUpdate.as_view()),
+    path('api/v1/peoplesdetail/<int:pk>/', PeopleAPIDetail.as_view())
 ]
